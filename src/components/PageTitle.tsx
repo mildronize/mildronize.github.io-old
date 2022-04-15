540f9815b502c7d7b83cb4f3b2a571dd65f19a34
Thada Wangthammang <thada.wth@gmail.com>
2022-04-15 14:21:25 +0700

    decorate tag page

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const PageTitle = styled.h2`
   color: var(--colors-text-0);
   font-family: var(--font-family-inter);
-  font-weight: 600;
+  font-weight: 500;
   line-height: 1.5;
   letter-spacing: 0.3px;
   font-size: 1.7rem;
--- src/templates/all-tags.jsx
+++ src/templates/all-tags.jsx
@@ -7,14 +7,13 @@ import PageTitle from "../components/PageTitle";
 
 export default function AllTag({ pageContext }) {
   const { sortedTags } = pageContext;
-  console.log(sortedTags);
   return (
     <Layout>
       <div>
         <Helmet title={`Posts tagged as  | ${config.siteTitle}`} />
         <PageTitle>All Tags</PageTitle>
         {sortedTags.map(tag => (
-          <Tag key={tag.slug}><a href={tag.path}>{tag.displayName} <sup>{tag.count}</sup></a></Tag>
+          <Tag key={tag.slug}><a className="zoom" href={tag.path}>{tag.displayName} <sup>{tag.count}</sup></a></Tag>
         ))}
       </div>
     </Layout>
@@ -28,10 +27,23 @@ const Tag = styled.span`
   }
   a {
     color: var(--color-default);
+    font-size: 1.2rem;
+    padding: 5px 15px;
+    font-weight: 400;
+    display: inline-block;
+  }
+  .zoom {
+    transition: all ease-out 0.2s;
+  }
+  .zoom:hover {
+    position: relative;
+    color: var(--colors-brand);
+    transform: scale(1.2);
+    font-weight: 500;
+    background: var(--colors-text-4);
+    border-radius: 10px;
+    z-index: 10;
   }
 
-  font-size: 1.2rem;
-  padding: 5px 15px;
-  font-weight: 400;
-  display: inline-block;
+
 `;
